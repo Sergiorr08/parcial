283a16a79ce3041eb6930c95ced9aa49bbbc468d
Sergiorr08 <kikoplo22@gmail.com>
2025-03-11 06:00:12 +0000

    Correcciones en Test

--- lambda_parser/test/test_csv.py
+++ lambda_parser/test/test_csv.py
@@ -1,13 +1,13 @@
 import pytest
 import boto3
-from moto import mock_s3
+from moto import mock_aws
 from lambda_parser.main import save_to_csv
 
 S3_BUCKET_OUTPUT = "test-bucket"
 
 @pytest.fixture
 def s3_mock():
-    with mock_s3():
+    with mock_aws():  # <--- Cambia mock_s3() por mock_aws()
         s3 = boto3.client("s3", region_name="us-east-1")
         s3.create_bucket(Bucket=S3_BUCKET_OUTPUT)
         yield s3
@@ -17,6 +17,6 @@ def test_save_to_csv(s3_mock):
         ["2025-03-10", "Chapinero", "$300,000,000", "1", "1", "40"]
     ]
     save_to_csv(test_data, "test_output.csv")
-    
+
     response = s3_mock.list_objects_v2(Bucket=S3_BUCKET_OUTPUT)
     assert "Contents" in response  # Verifica que el archivo fue subido
